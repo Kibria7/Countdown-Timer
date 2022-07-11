3bf9c655bd24e6976ca7cd5a56d618c5df67470e
Golam Kibria <golam15-14592@diu.edu.bd>
2022-07-11 21:57:30 +0600

    changed it back to showing the timer for new years eve

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = '12 Sep 2022';
+const newYears = '1 Jan 2023';
 
 function countdown(){
     const newYearsDate = new Date(newYears);
