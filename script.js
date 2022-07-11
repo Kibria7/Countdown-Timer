c51f728d9d57a45e6b89baa4dd94347657ce1597
Golam Kibria <golam15-14592@diu.edu.bd>
2022-07-11 23:11:49 +0600

    minor change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = '23 Sep 2022';
+const newYears = '23 Sep 2022git ';
 
 function countdown(){
     const newYearsDate = new Date(newYears);
