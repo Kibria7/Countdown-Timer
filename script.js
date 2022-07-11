8a87912d4615944107f783605c11175f93d14e47
Golam Kibria <golam15-14592@diu.edu.bd>
2022-07-11 22:58:13 +0600

    Changed the timer to another friend's birthday

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = '1 Jan 2023';
+const newYears = '23 Sep 2023';
 
 function countdown(){
     const newYearsDate = new Date(newYears);
